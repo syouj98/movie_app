765b3154bac2fcf62a566ee41f86c479ab401012
syouj98 <yj980514@gmail.com>
2024-04-14 18:34:11 +0900

    최종 완성 코드

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -2,7 +2,7 @@ import React from "react";
 
 class Detail extends React.Component{
     componentDidMount(){
-        const { location, history }=this.props; //구조 분해 할당으로 location, history를 얻음
+        const { location, history }=this.props; // 구조 분해 할당으로 location, history를 얻음
         if(location.state === undefined){
             history.push('/');
         }
